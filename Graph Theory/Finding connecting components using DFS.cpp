b57801b74db83237c061e3bac71f310b6161bea4
EimuBarua <56397913+EimuBarua@users.noreply.github.com>
2020-12-19 12:04:59 +0600

    Create Finding connecting components using DFS.cpp

---
+++ Graph Theory/Finding connecting components using DFS.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define soja(i,a,n) for(ll i=a;i<=n;i++)
+#define ulta(i,n,a) for(ll i=n;i>=a;i--)
+#define pb push_back
+const ll mx=1000;
+vector<bool>vis(mx+1,0);
+vector<ll>adj[mx];
+void dfs(ll a)
+{
+  //cout<<a<<endl;
+vis[a]=1;
+for(auto i:adj[a])
+{
+  if(vis[i]==0)
+  dfs(i);
+}
+}
+int main() {
+    ll node,edge,a,b,sum=0;
+  cin>>node>>edge;
+  soja(i,0,edge-1)
+  {
+    cin>>a>>b;
+    adj[a].pb(b);
+    adj[b].pb(a);
+  }
+  soja(i,1,node)
+  {
+      if(vis[i]==0)
+      {
+          sum++;
+          dfs(i);
+      }
+  }
+  cout<<sum<<endl;
+    return 0;
+}
