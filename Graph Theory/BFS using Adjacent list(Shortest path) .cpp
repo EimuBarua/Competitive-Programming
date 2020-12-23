b6c8a605b393b287c5daa169cf004de270d82c46
EimuBarua <56397913+EimuBarua@users.noreply.github.com>
2020-12-23 11:03:33 +0600

    Update BFS using Adjacent list(Shortest path) .cpp

--- Graph Theory/BFS using Adjacent list(Shortest path) .cpp
+++ Graph Theory/BFS using Adjacent list(Shortest path) .cpp
@@ -4,71 +4,46 @@ using namespace std;
 #define soja(i,a,n) for(ll i=a;i<=n;i++)
 #define ulta(i,n,a) for(ll i=n;i>=a;i--)
 #define pb push_back
-ll bfs(ll node,ll edge,ll st,ll dt,vector<ll>adj[],vector<ll>&d,vector<ll>parent,vector<ll>&path)
+vector<ll>adj[1000],dis(1000,-1);
+ll bfs(ll n)
 {
-    ll k;
-    vector<bool>vis(node+1,0);
     queue<ll>q;
-    q.push(st);
-
+    ll k=0;
+    dis[n]=0;
+    q.push(n);
     while(1)
     {
-        if(q.empty()==1)
-        break;
-        k=q.front();
-        vis[k]=1;
-        q.pop();
-        soja(i,0,adj[k].size()-1)
+        if(!q.size())
+            break;
+            n=q.front();
+            q.pop();
+           // cout<<n<<endl;
+        for(auto i:adj[n])
         {
 
-            if(vis[adj[k][i]]==0&&k<adj[k][i])
-            {q.push(adj[k][i]);
-            d[adj[k][i]]=d[k]+1;
+            if(dis[i]==-1)
+            {
+                dis[i]=dis[n]+1;
+                q.push(i);
             }
+            dis[i]=min(dis[i],dis[n]+1);
         }
     }
-    if(vis[dt]==0)
-        return 0;
-    k=dt;
-    bool fg=0;
-        while(1)
-    {
-        if(k==-1)
-            break;
-            if(fg==0)
-        path.pb(k);
-        if(k==st)
-            fg=1;
-        k=parent[k];
-    }
-    if(fg==0)
-    return 0;
-    else
-        return 1;
 }
 int main()
 {
-    ll node,edge,a,b,st,dt;
-    cin>>node>>edge>>st>>dt;
-    vector<ll>d(node+1,0);
-    vector<ll>path;
-    vector<ll>adj[node+1];
-    vector<ll>parent(node+1,-1);
-    soja(i,1,edge)
+    ll n,m,k,t,sum=0;
+    ll node,edge;
+    cin>>node>>edge;
+    soja(i,0,edge-1)
     {
-        cin>>a>>b;
-        adj[a].pb(b);
-        adj[b].pb(a);
-        parent[b]=a;
+        cin>>n>>m;
+        adj[n].pb(m);
+        adj[m].pb(n);
     }
-    if(bfs(node,edge,st,dt,adj,d,parent,path)==0)
-        cout<<"NO path"<<endl;
-    else
-    {
-    cout<<d[dt]<<endl;
-ulta(i,path.size()-1,0)
-cout<<path[i]<<" ";
-cout<<endl;
-    }
+    bfs(1);
+    soja(i,1,node)
+    cout<<dis[i]<<" ";
+    cout<<endl;
     return 0;
 }
