4ad5ce3c27333c063e373b9acc5ead649dbb2d39
EimuBarua <56397913+EimuBarua@users.noreply.github.com>
2021-03-09 22:14:06 +0600

    Update lower_bound.cpp

--- searching/lower_bound.cpp
+++ searching/lower_bound.cpp
@@ -1,51 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
-#define soja(i,a,n) for(ll i=a;i<=n;i++)
-#define ulta(i,n,a) for(ll i=n;i>=a;i--)
-#define pb push_back
-vector<ll>v;
-//finding lower bound
-//lower bound is the left most index with value greater /equal to a particular number
-ll lower_search(ll n,ll m)
-{
-    ll l=0,r=n-1,mid;
-    bool fg=0;
-    while(1)
-    {
-            mid=(l+r)/2;
-        if(v[mid]==m)
-        {fg=1;
-            break;}
-        if(l>r)
-            break;
-        if(v[mid]>m)
-        {
-            r=mid-1;
-        }
-        else
-            l=mid+1;
-    }
-    return l;
-}
+
 int main()
 {
-    ll n,m,k,t,sum=0;
-    cin>>n;
-    soja(i,0,n-1)
-{cin>>k;
-v.pb(k);}
-v.pb(0);
-    cin>>m;
-    k=lower_search(n,m);
+    //10 20 20 40 50
+    //20
+    ll n,key;
+    cin>>n>>key;
+    ll a[n];
+    bool fg=0;
+    ll index=-1;
+    for(ll i=0;i<n;i++)
+        cin>>a[i];
+    sort(a,a+n);
+   ll lo=0,hi=n-1;
+   ll mid;
+   while(lo<=hi)
+   {
+       mid=(lo+hi)/2;
+       if(a[mid]==key)
+       {
+           hi=mid-1;
+       }
+       else if(a[mid]<key)
+       {
+           lo=mid+1;
+       }
+       else if(a[mid]>key)
+       {
+           hi=mid-1;
+       }
 
-    ulta(i,n,k+1)
-    {
-        v[i]=v[i-1];
-    }
-    v[k]=m;
-    soja(i,0,v.size()-1)
-    cout<<v[i]<<" ";
-    cout<<endl;
+   }
+   cout<<lo<<endl;
     return 0;
 }
