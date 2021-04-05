16a968d6aee3e93e835292bdbff86987ae4033a7
EimuBarua <56397913+EimuBarua@users.noreply.github.com>
2021-04-06 01:01:19 +0600

    Update Dijkstra.cpp

--- Graph Theory/Dijkstra.cpp
+++ Graph Theory/Dijkstra.cpp
@@ -4,50 +4,46 @@ using namespace std;
 #define soja(i,a,n) for(ll i=a;i<=n;i++)
 #define ulta(i,n,a) for(ll i=n;i>=a;i--)
 #define pb push_back
-const ll inf =1e9+7;
-ll n,m,k,t,sum=0,node,edge,cost,src,minnode;
-ll adj[1000][1000];
+const ll inf =1e18;
+ll n,m,k,t,sum=0,node,edge,cost,src,minnode,u,w;
+vector<pair<ll,ll>> adj[100005];
 void dij(ll src)
 {
-    //making all dis inf
-    vector<ll>dis(node+1,inf);
-    //making all nodes unvisited
-    vector<ll>vis(node+1,0);
-    //make the src node 0 
-    dis[src]=0;
-    //finding for all the nodes except the last node
-    soja(i,0,node-2)
-    {
-        k=inf,minnode=0;
-        //finding the minimum dist from all the unvisited nodes
-        soja(j,0,node-1)
+        vector<ll>dis(node+1,inf),parent(node+1,-1);
+        set<pair<ll,ll>>q;
+        dis[src]=0;
+        q.insert({dis[src],src});
+        while(!q.empty())
         {
-            if(vis[j]==0&&dis[j]<=k)
-                k=dis[j],minnode=j;
+            k=q.begin()->second;
+            q.erase(q.begin());
+            for(auto x:adj[k])
+            {
+                u=x.first;
+                w=x.second;
+                if(dis[k]+w<dis[u])
+                    {
+                        q.erase({dis[u],u});
+                        dis[u]=dis[k]+w;
+                        parent[u]=k;
+                         q.insert({dis[u],u});
+                    }
+            }
+
         }
-        // making the found node visit
-        vis[minnode]=1;
-        //changing all the unvisited nodes distance 
-        soja(j,0,node-1)
-        {
-            if(vis[j]==0&&adj[minnode][j]!=-1&&(dis[minnode]+adj[minnode][j])<dis[j])
-                dis[j]=dis[minnode]+adj[minnode][j];
-        }
-    }
-    soja(i,0,node-1)
-    cout<<dis[i]<<" ";
-    cout<<endl;
+
 }
+
 int main()
 {
     cin>>node>>edge;
-    src=0;
-    memset(adj,-1,sizeof(adj));
+    src=1;
     soja(i,0,edge-1)
     {
         cin>>n>>m>>cost;
-        adj[n][m]=cost;
-          adj[m][n]=cost;
+     adj[n].pb({m,cost});
+     adj[m].pb({n,cost});
     }
+
     dij(src);
 }
