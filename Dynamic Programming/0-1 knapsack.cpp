c4a2d429f67b80ff3a656756313796fc7baa23c3
EimuBarua <56397913+EimuBarua@users.noreply.github.com>
2021-07-08 19:31:55 +0600

    Update 0-1 knapsack.cpp

--- Dynamic Programming/0-1 knapsack.cpp
+++ Dynamic Programming/0-1 knapsack.cpp
@@ -1,30 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long int
+#define ll int
 #define soja(i,a,n) for(ll i=a;i<=n;i++)
 #define ulta(i,n,a) for(ll i=n;i>=a;i--)
 #define pb push_back
-ll n,m,k,t,sum=0,bag;
-ll value[100000],weight[100000];
-ll dp[1000][100005];
-//elements*sizeofbag
-ll f(ll i,ll p)
-{
-    if(i>n)
-        return 0;
-    if(dp[i][p]>0)
-        return dp[i][p];
-    ll res=f(i+1,p);
-    if(p+weight[i]<=bag)
-        res=max(res,value[i]+f(i+1,p+weight[i]));
-        return dp[i][p]=res;
-
-}
+#define line cout<<"\n"
+void debugg(){cout<<"##Debug ";}
+template<typename T>void print(T x){cout<<x<<" ";}
+template<typename T> void debug(T x){debugg();print(x);line;}
+template<typename T> void debug(T a,T b){debugg();print(a);print(b);line;}
+template<typename T> void debug(T a,T b,T c){debugg();print(a);print(b);print(c);line;}
+template<typename T> void debug(T a,T b,T c,T d){debugg();print(a);print(b);print(c);print(d);line;}
+template<typename T> void debugarray(T v,ll n){debugg();soja(i,0,n-1)print(v[i]);line;}
+ll a[1001],b[1001];
+ll dp[1002][100002];
+ 
 int main()
 {
-    cin>>n>>bag;
+    ll n,m,k,t,sum=0;
+    cin>>n>>m;/// m = maximum weight
     soja(i,1,n)
-    cin>>weight[i]>>value[i];
-    cout<<f(1,0)<<endl;
+    cin>>a[i];///weight array
+    soja(i,1,n)
+    cin>>b[i];///value array
+    soja(i,1,n)
+    {
+        soja(j,0,m)
+        {
+            if(j>=a[i])
+            dp[i][j]=max(dp[i-1][j],dp[i-1][j-a[i]]+b[i]);
+            else
+                dp[i][j]=dp[i-1][j];
+        }
+    }
+   cout<<dp[n][m];
+    line;
     return 0;
 }
