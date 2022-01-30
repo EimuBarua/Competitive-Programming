248004c615958e3ed1f72eb222a2d82629d702c5
Eimu Barua <eimubarua39@gmail.com>
2022-01-30 11:27:29 +0600

    Update all mod.cpp

--- Number Theory/all mod.cpp
+++ Number Theory/all mod.cpp
@@ -1,4 +1,7 @@
-ll ad(ll x,ll y){ll f=((x%mod)+(y%mod)+mod)%mod;return f;}
-ll sub(ll x,ll y){ll f=((x%mod)-(y%mod)+mod)%mod;return f;}
-ll mul(ll x,ll y){ll f=((x%mod)*(y%mod)+mod)%mod;return f;}
-ll bigmod(ll n,ll m){ll f=1;while(m){if(m%2==1)f=((f%mod)*(n%mod))%mod;n=((n%mod)*(n%mod))%mod;m/=2;}return (f%mod+mod)%mod;}
+void modnor(ll &x) {x %= mod; if(x < 0)(x += mod);}
+ll modmul(ll x, ll y) { x %= mod, y %= mod; modnor(x),modnor(y); return (x*y)%mod; }
+ll modadd(ll x, ll y) { x %= mod, y %= mod; modnor(x),modnor(y); return (x+y)%mod; }
+ll modsub(ll x, ll y) { x %= mod, y %= mod; modnor(x),modnor(y); x -= y; modnor(x); return x; }
+ll modpow(ll b, ll p) { ll r = 1; while(p) {if(p&1) r = modmul(r, b); b = modmul(b, b);p >>= 1;}return r;}
+ll modinverse(ll x){return modpow(x,mod-2);}  /// if mod is prime.
+ll moddiv(ll x, ll y){return modmul(x,modinverse(y));}
