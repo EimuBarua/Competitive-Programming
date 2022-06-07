7fca69a4af7594b42f6ff5e8f74352050948e5c6
Eimu Barua <eimubarua39@gmail.com>
2022-06-07 11:38:51 +0600

    Update Dijkstra.cpp

--- Graph Theory/Dijkstra.cpp
+++ Graph Theory/Dijkstra.cpp
@@ -5,29 +5,28 @@ using namespace std;
 #define ulta(i,n,a) for(ll i=n;i>=a;i--)
 #define pb push_back
 const ll inf =1e18;
-const ll mx=1e5+7;
 ll n,m,k,t,sum=0,node,edge,cost,src,minnode,u,w;
-vector<pair<ll,ll>> adj[mx];
-ll dis[mx];
-ll parent[mx];
+vector<pair<ll,ll>> adj[100005];
 void dij(ll src)
 {
-       priority_queue<pair<ll,ll>>q;
+        vector<ll>dis(node+1,inf),parent(node+1,-1);
+        set<pair<ll,ll>>q;
         dis[src]=0;
-        q.push({dis[src],src});
+        q.insert({dis[src],src});
         while(!q.empty())
         {
-            k=q.top().second;
-            q.pop();
+            k=q.begin()->second;
+            q.erase(q.begin());
             for(auto x:adj[k])
             {
                 u=x.first;
                 w=x.second;
                 if(dis[k]+w<dis[u])
                     {
+                        q.erase({dis[u],u});
                         dis[u]=dis[k]+w;
                         parent[u]=k;
-                        q.push({dis[u],u});
+                         q.insert({dis[u],u});
                     }
             }
 
@@ -37,11 +36,6 @@ void dij(ll src)
 
 int main()
 {
-	soja(i,0,mx-1)
-	{
-		dis[i]=inf;
-		parent[i]=-1;
-	}
     cin>>node>>edge;
     src=1;
     soja(i,0,edge-1)
@@ -52,17 +46,4 @@ int main()
     }
 
     dij(src);
-	/*
-    soja(i,1,node)
-    {
-    	cout<<"DIS  "<<i<<" "<<dis[i]<<endl;
-    	k=i;
-    	cout<<"path of "<<i<<endl;
-    	while(parent[k]!=-1)
-    	{
-    		cout<<k<<" ";
-    		k=parent[k];
-    	}
-    	cout<<endl;
-    }*/
 }
