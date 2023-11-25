ad75ab4f04d91b962b148e150050b3abc4affc20
Eimu Barua <eimubarua39@gmail.com>
2023-11-25 12:27:02 +0600

    Update Prime_Factorization.cpp

--- Number Theory/Prime_Factorization.cpp
+++ Number Theory/Prime_Factorization.cpp
@@ -1,52 +1,55 @@
+
+// primefactor in O(logn) but have to use seive
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long int
+#define ll int
 #define soja(i,a,n) for(ll i=a;i<=n;i++)
 #define ulta(i,n,a) for(ll i=n;i>=a;i--)
 #define pb push_back
-vector<ll>prime;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+const ll mx=1e7+1;
+ll vis[mx+5];
 void seive()
 {
-    ll n=1e7;
-    vector<bool>vis(n,0);
-    for(ll i=3;i*i<=n;i+=2)
+    for(ll i=1;i<=mx;i++)vis[i]=1e9;
+    vis[1]=1;
+    for(ll i=3;i*i<=mx;i+=2)
     {
-        if(vis[i]==0)
+        if(vis[i]==1e9)
         {
-        for(ll j=i*i;j<n;j+=2*i)
-        vis[j]=1;}
+        vis[i]=i;
+        for(ll j=i*i;j<mx;j+=2*i)
+        vis[j]=min(i,vis[j]);}
     }
-    prime.pb(2);
-    for(ll i=3;i<n;i+=2)
+    for(ll i=3;i<=mx;i+=2)
     {
-        if(vis[i]==0)
-            prime.pb(i);
+        if(vis[i]==1e9)vis[i]=i;
     }
+    for(ll i=2;i<=mx;i+=2)vis[i]=2;
+
 }
-ll primefactor(ll n,vector<ll>&a)
+vector<ll>primefactor(ll n)
 {
-    ll m=sqrt(n);
-    soja(i,0,prime.size()-1)
-    {
-        while(n%prime[i]==0)
+
+    vector<ll>v;
+        while(vis[n]!=n)
         {
-            a.pb(prime[i]);
-            n/=prime[i];
+        v.pb(vis[n]);
+            n/=vis[n];
         }
-        if(n==1||prime[i]>m)
-            break;
-            m=sqrt(n);
-    }
-    if(n>1)
-        a.pb(n);
+        if(n>1)
+           v.pb(n);
+        return v;
 }
 int main()
 {
+    fast;
     seive();
-    ll n;
-    vector<ll>a;
+    ll n,t;
     cin>>n;
-    primefactor(n,a);
-    soja(i,0,a.size()-1)
-    cout<<a[i]<<" ";
+    vector<ll>v=primefactor(n);
+   for(auto xx:v)cout<<xx<<" ";
+   cout<<endl;
+
+
 }
